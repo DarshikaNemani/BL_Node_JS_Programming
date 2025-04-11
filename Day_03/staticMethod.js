1ea8eeec9605f4136e1874bb0b7926b63b7a9537
DarshikaNemani <dk5673@srmist.edu.in>
2025-04-11 15:36:47 +0530

    Static Method example

--- Day_03/staticMethod.js
+++ Day_03/staticMethod.js
@@ -4,11 +4,16 @@ class Person {
         console.log("constuctor: ", this.name);
     }
 
-    static Name(name){
+    static Name(name){ // Using Static keyword
         console.log("Static:", name);
     }
 }
 
+Person.Name2 = function(name2){ // Without static keyword
+    console.log("Dusra static: ", name2);
+}
+
 const person1 = new Person ("Darshika")
 Person.Name("ABCD");
+Person.Name2("XYZ")
 person1.Name("ABCD"); //Gives error because static belongs to class and not object.
