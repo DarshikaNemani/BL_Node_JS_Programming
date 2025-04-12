362262af1e51529f421b1d093e551c5ac6cf736e
DarshikaNemani <dk5673@srmist.edu.in>
2025-04-12 11:35:18 +0530

    If-Else practice question 2

---
+++ Day_04/grade.js
@@ -0,0 +1,17 @@
+let x = "75";
+
+if(x >= 90){
+    console.log('"A" aa gaya oye hoyeeee!');
+}
+else if(x >= 70 && x < 90){
+    console.log('"B" toh sahi hi hai!');
+}
+else if (x >= 50 && x < 70){
+    console.log('"C" se pass toh ho gai na!')
+}
+else if (x < 50){
+    console.log("Uhhh ghar wale nikaal denge?");
+}
+else{
+    console.log("Koi sense hai?")
+}
