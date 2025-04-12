f877e2f53fb953e83c3e24f08ec82d39a4484493
DarshikaNemani <dk5673@srmist.edu.in>
2025-04-12 11:29:47 +0530

    If-Else practice question

---
+++ Day_04/plusMinusZero.js
@@ -0,0 +1,14 @@
+let x = "A";
+
+if(x < 0){
+    console.log("The number is negative.");
+}
+else if(x > 0){
+    console.log("The number is positive.");
+}
+else if (x == 0){
+    console.log("The number is zero.")
+}
+else{
+    console.log("Bhai number daalna!!!");
+}
